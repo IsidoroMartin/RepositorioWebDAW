e34bc3cba3106b31974837d6f62b32fe08a78c92
Juanjo Ramírez Sánchez <juanjoramirezsanchez@gmail.com>
2016-01-05 00:48:04 +0100

    Actualizada página de contacto, actualizado imagenes de rutas, cambios en organizacion de fonts-awesome

--- js/formulario.js
+++ js/formulario.js
@@ -7,102 +7,90 @@
  */
 
 /*
-  Copyright (C) 2015 yesica alexandra rojas
- 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
- 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
- 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ Copyright (C) 2015 yesica alexandra rojas
 
-//funcion que devuelve true si todas las validaciones del formulario son correctas 
-/*function validar() {
-	if (ValidaCampo("nombre") && ValidaCampo("primerApellido") && ValidaCampo("segundoApellido")  && validaTelefonoFijo() && validaTelefonoMovil() &&  validaAsunto()) {
-		return true
-	}
-	return false
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//listas
+//funcion que rellena la lista asunto uando seleccionas una opcion del tipo de cosulta
+function rellenarLista(frm) {
+
+    var consulta = document.getElementById("tipoConsulta").selectedIndex
+    if (consulta > 0) {
+        frm.asunto.disabled = false
+
+    } else {
+        frm.asunto.disabled = true
+
+    }
+
+    var arraytextos = new Array(4)
+    arraytextos["consulta"] = ["", "", "", "", ""]
+    arraytextos["sugerencia"] = ["Sobre la web", "Sobre guias", "Sobre Rutas", "Otros..."]
+    arraytextos["queja"] = ["Sobre la web", "Sobre organización", "Otros..."]
+    arraytextos["informacion"] = ["Sobre rutas", "Sobre guias", "Fotos", "Otros..."]
+
+    var tipoConsulta = document.getElementById("tipoConsulta")
+    var asunto = document.getElementById("asunto")
+    var indiceSeleccionado = tipoConsulta.options[tipoConsulta.selectedIndex].value
+
+    while (asunto.options.length) {
+        asunto.remove(0)
+    }
+    var consulta = arraytextos[indiceSeleccionado]
+    if (consulta) {
+        for (var i = 0; i < consulta.length; i++) {
+            var asuntoi = new Option(consulta[i], i)
+            asunto.options.add(asuntoi)
+        }
+    }
+    activarBoton(frm)
 }
 
-/*
-//funcion que valida si el campo  esta vacio
-function ValidaCampo(id) {
-	var apellido=document.getElementById(id).value
-	if (apellido.length==0 || apellido=="") {
-		alert(" el campo esta vacio")	
-		return false
-	}
-	return true
-}
-
-//funcion que valida si el campo telefonoFijo tiene el formato correcto (el numero tiene que empezar por 9 y 8 digitos mas)
-function validaTelefonoFijo() {
-	var telefono=document.getElementById("telefonoFijo").value
-	if (/^[9]\d{8}$/.test(telefono)) {
-		return true
-	}
-	document.getElementById("telefonoFijo").value=""
-	alert("telefono No valido debe empezar por 9")
-return false
-}
-
-//funcion que valida si el campo telefonoMovil tiene el formato correcto (el numero tiene que empezar por 6  y 8 digitos mas)
-function validaTelefonoMovil() {
-	var telefono=document.getElementById("telefonoMovil").value
-	if (/^[6]\d{8}$/.test(telefono)) {
-		return true
-	}
-	document.getElementById("telefonoMovil").value=""
-	alert("telefono No valido debe empezar por 6 ")
-return false
-}
-
-//funcion que valiada si se ha escogido alguna opcion que no sea la 0 
-function validaAsunto() {
-	var asunto=document.getElementById("asunto").selectedIndex
-	if (asunto==0) {
-		alert("Asunto no puede quedar vacio porfavor seleccione un asunto")
-		return false
-	}
-	return true
-}
-*/
-
-//funcion que habilita la casilla terminos (checkbox) si estan todos los campos rellenos
+// funcion que habilita la casilla terminos (checkbox) si estan todos los campos
+// rellenos
 function habilitarTerminosYCondiciones(frm) {
-	var nombre=document.getElementById("nombre").value
-	var apellido1=document.getElementById("primerApellido").value
-	var apellido2=document.getElementById("segundoApellido").value
-	var email=document.getElementById("email").value
-	var telefonoMovil=document.getElementById("telefonoMovil").value
-	var descripcion=document.getElementById("descripcion").value
-	var asunto=document.getElementById("asunto").selectedIndex
-	
-	
-	if (nombre!="" && apellido1!="" && apellido2!="" && email!="" && telefonoMovil!=""  && descripcion!=""  && asunto>0) {
-		//alert("habilitado")
-		frm.terminos.disabled=false
-		return true
-	}
-	frm.terminos.disabled=true
-	return false
+
+    var nombre = document.getElementById("nombre").value
+    var apellido1 = document.getElementById("primerApellido").value
+    var apellido2 = document.getElementById("segundoApellido").value
+    var email = document.getElementById("email").value
+    var telefonoMovil = document.getElementById("telefonoMovil").value
+    var descripcion = document.getElementById("descripcion").value
+    var consulta = document.getElementById("tipoConsulta").selectedIndex
+    var asunto = document.getElementById("asunto").selectedIndex
+
+    if (nombre != "" && apellido1 != "" && apellido2 != "" && email != ""
+        && telefonoMovil != "" && descripcion != "" && consulta != 0
+        && asunto != 0) {
+        //alert("habilitado")
+        frm.terminos.disabled = false
+        return true
+    }
+    frm.terminos.disabled = true
+    return false
 }
 
-//funcion que habilita el boton si estan todos los campos rellenos y la casillla de verificacion esta chekeada
+// funcion que habilita el boton si estan todos los campos rellenos y la
+// casillla de verificacion esta chekeada
 function activarBoton(frm) {
-	
-	var terminos=document.getElementById("terminos")
-	if (habilitarTerminosYCondiciones(frm) && terminos.checked) {
-		frm.envio.disabled=false
-	}else{
-		frm.envio.disabled=true
-	}
-		
+    var terminos = document.getElementById("terminos")
+    if (habilitarTerminosYCondiciones(frm) && terminos.checked) {
+        frm.envio.disabled = false
+    } else {
+        frm.envio.disabled = true
+    }
+
 }
